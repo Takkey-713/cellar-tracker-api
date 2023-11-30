c6bf3557d3f00179e886ced230f8963de63c18b4
Takkey-713 <takiyu713@gmail.com>
2023-12-01 02:21:31 +0900

    fix: 関数ベースなのにControllerがあるのに違和感を感じたためhandlerに修正

--- src/logics/users/userJwt.ts
+++ src/logics/users/userJwt.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import { User } from '../../models/user'
 
 export const userJwt = (user: User) => {
-  const secretKey = process.env.JWT_SECRET_KEY || 'oLq4EgnpXKZS+8uPfn4eEZL4eIA8FI72nniCr4MmO5w='
+  const secretKey = process.env.JWT_SECRET_KEY as string
   const payload = {
     id: user.id,
     email: user.email,

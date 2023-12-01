ce247ac3c6db5153c4ee6696cd0bb766848da30a
Takkey-713 <takiyu713@gmail.com>
2023-12-01 15:10:29 +0900

    add: coreの設定を追加

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from 'express'
+import cors from 'cors'
 import router from './routers/router'
 
 const app = express()
-const port = process.env.PORT || 5001
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
 
+app.use(cors(corsOptions))
+const port = process.env.PORT || 5001
 app.use(express.json())
+app.use('/api/v1', router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-app.use('/api/v1', router)
